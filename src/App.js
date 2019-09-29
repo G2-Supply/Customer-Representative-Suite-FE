33469ff4168e10d1cde7165ca7a08e3a515708fe
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-29 00:36:18 -0500

    working on initial styling for Side and TopNav, switching to navigation branch after this push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 // component imports 
 import AllNav from './components/Navigation/AllNav';
--- src/components/Navigation/SideNav.jsx
+++ src/components/Navigation/SideNav.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'; 
+
+// styling import
+import './SideNav.scss'
 
 const SideNav = () => {
     return ( 
-        <>
-            <p>SideNav</p>
-        </>
+        <div className="sidenav-wrapper">
+            <p className="signout">Sign Out</p>
+            <NavLink className='side-links' to='/box-calculator'>Box Calculator >>
+                <p className="link-desc">
+                    - An ineractive way to determine
+                    the square footage of corrugated
+                    boxes for pricing purposes.
+                </p>
+            </NavLink>
+            <NavLink className='side-links' to='/saved-boxes'>My Saved Boxes >>
+                <p className="link-desc">
+                    - A list of vendor and contact info for
+                    easy access.
+                </p>
+            </NavLink>
+            <NavLink className='side-links' to='/products'>Products >>
+                <p className="link-desc">
+                    - A searchable list of all of the 
+                    products we offer.
+                </p>
+            </NavLink>
+            <NavLink className='side-links' to='/vendor-list'>Vendor List >>
+                <p className="link-desc">
+                    - A list of vendor and contact info for 
+                    easy access.
+                </p>
+            </NavLink>
+        </div>
      );
 }
  
