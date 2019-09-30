80cdb842fb28c5c3a827b533d59d42adeedf28c2
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-29 19:42:03 -0500

    initial form setup and styled.  Need to get basic Mongo database setup before I can continue writing functionality for the Box Calc tool

--- src/components/BoxCalculator/BoxCalculator.jsx
+++ src/components/BoxCalculator/BoxCalculator.jsx
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// importing axios for http requests
+import axios from 'axios'; 
+
+// styling imports 
+import './BoxCalculator.scss'; 
 
 const BoxCalculator = () => {
+
+    // useState hook for form logic
+    const [ form, setForm ] = useState({
+        name: '',
+        length: '',
+        width: '',
+        height: '',
+    }); 
+
+    // pulling the list of possible box formulas from the API on page render, going to save it to an array
+    useEffect(() => {
+        axios.get('formulaArray')
+    }, [])
+
+    // handling form logic with a useState hook
+    const handleChange = (e) => {
+        setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+    })}
+
+    // the function that actually handles the calculation of the square footage
+    const calculateFootage = () => {
+
+    }
+
+    // function to POST the new box the the API and save it to the database
+    const saveBox = () => {
+
+    }
+
     return ( 
-        <div>
-            <h1>Box Calculator</h1>
-        </div>
+        <>
+            <form className="boxcalc-wrapper">
+                <label>Box Name:<br/>
+                    <input type="text"
+                    name="name"
+                    onChange={handleChange}
+                    value={form.name}
+                    className="boxcalc-field"
+                    />
+                </label>
+                <label>Length:<br/>
+                    <input type="number"
+                    name="length"
+                    onChange={handleChange}
+                    value={form.length}
+                    className="boxcalc-field"
+                    />
+                </label>
+                <label>Width:<br/>
+                    <input type="number"
+                    name="width"
+                    onChange={handleChange}
+                    value={form.width}
+                    className="boxcalc-field"
+                    />
+                </label>
+                <label>Height:<br/>
+                    <input type="number"
+                    name="height"
+                    onChange={handleChange}
+                    value={form.height}
+                    className="boxcalc-field"
+                    />
+                </label>
+                <button className="calculate-box" onClick={calculateFootage}>Calculate</button>
+                <label>Square Footage:<br/>
+                    <input 
+                    type="number"
+                    name="footage"
+                    className="boxcalc-field"
+                    />
+                </label>
+                <p className="save-question">Need to save this box?</p>
+                <div className="save-btn-wrapper">
+                    <p className="no-thanks">No thanks</p>
+                    <button className="save-btn" onClick={saveBox}>Save</button>
+                </div>
+            </form>
+        </>
      );
 }
  
--- src/components/Navigation/SideNav.jsx
+++ src/components/Navigation/SideNav.jsx
@@ -4,7 +4,12 @@ import { NavLink } from 'react-router-dom';
 // styling import
 import './SideNav.scss'
 
-const SideNav = () => {
+const SideNav = (props) => {
+
+    // function signOut() {
+    //     localStorage.removeItem('token'); 
+    //     props.history.push('/login'); 
+    // }
     return ( 
         <div className="sidenav-wrapper">
             <p className="signout">Sign Out</p>
